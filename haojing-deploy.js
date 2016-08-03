8dd172dfcadab8d1cfc83a6e7383bf648242467a
stdrickforce <stdrickforce@gmail.com>
2016-08-03 17:06:06 +0800

    haojing deploy

---
+++ haojing-deploy.js
@@ -0,0 +1,1 @@
+configureNotification({'icon': 'https://cn.gravatar.com/userimage/55433269/8f0df807a9018d3520403f31534fbff2.jpg?size=200', 'sound': "https://stdrickforce.github.io/static/Track2.mp3"})
